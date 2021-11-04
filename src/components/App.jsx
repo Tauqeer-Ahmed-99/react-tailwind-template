3fa5ab9bcb9a8f43d1ff2d84284e3e9f507fd73d
Tauqeer-Ahmed-99 <tauqueerrkhan@gmail.com>
2021-11-05 01:53:19 +0530

    ready to use template

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="flex flex-col items-start justify-center h-screen p-20 bg-gray-900">
+      <h1 className="text-5xl text-white">Hello Tailwind 👋</h1>
+      <p className="mt-5 text-lg text-gray-400">Start Creating your app...</p>
+      <button className="p-4 mt-5 font-bold text-white bg-green-600 rounded-lg hover:bg-gray-600">
+        Hello Friends 🚀
+      </button>
+    </div>
+  );
+};
+
+export default App;
